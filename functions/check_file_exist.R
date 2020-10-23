d2c717d833e1761e6b33a69250df517dcbd574e8
anais47 <anais.rey47@hotmail.fr>
2020-10-23 18:08:15 +0200

    add_jupyter_nb

--- functions/check_file_exist.R
+++ functions/check_file_exist.R
@@ -5,33 +5,33 @@ check_file_exist <- function(otu, metadata, taxo, trait, phylo){
   if(file.exists(paste("./", "input/", otu, ".csv", sep=""))){
     print("OTU file exists in the provided path")
   } else {
-    print("OTU file does not exist in the provided path")
+    print("ERROR: OTU file does not exist in the provided path")
   }
   
   if(file.exists(paste("./", "input/", metadata, ".csv", sep=""))){
     print("Sample metadata file exists in the provided path")
   } else {
-    print("Sample metadata file does not exist in the provided path")
+    print("ERROR: Sample metadata file does not exist in the provided path")
   }
   
   if(file.exists(paste("./", "input/", taxo, ".csv", sep=""))){
     print("Taxonomic assignment file exists in the provided path")
   } else {
-    print("Taxonomic assignment file doest not exist in the provided path")
+    print("ERROR: Taxonomic assignment file doest not exist in the provided path")
   }
   
   if(!is.null(paste("./", "input/", trait, ".csv", sep="")) &
      file.exists(paste("./", "input/", trait, ".csv", sep=""))){
     print("Trait table file exists in the provided path")
   } else {
-    print("Trait table file does not exist in the provided path --> If the user did not provide a trait table file it is OK, if not, there is an issue")
+    print("ERROR: Trait table file does not exist in the provided path --> If the user did not provide a trait table file it is OK, if not, there is an issue")
   }
   
   if(!is.null(paste("./", "input/", phylo, ".csv", sep="")) &
      file.exists(paste("./", "input/", phylo, ".csv", sep=""))){
     print("Phylogenetic table file exists in the provided path")
   } else {
-    print("Phylogenetic tree file does not exist in the provided path --> If the user did not provide a phylogenetic tree it is OK, if not, there is an issue")
+    print("ERROR: Phylogenetic tree file does not exist in the provided path --> If the user did not provide a phylogenetic tree it is OK, if not, there is an issue")
   }
   
 }
